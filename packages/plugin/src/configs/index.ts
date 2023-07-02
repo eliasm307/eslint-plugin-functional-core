e243c0139faae156fe63bf4b20985e495ec46c18
Eli <13420359+eliasm307@users.noreply.github.com>
2023-07-02 04:42:07 +0100

    put configs in the same file

--- packages/plugin/src/configs/index.ts
+++ packages/plugin/src/configs/index.ts
@@ -1,7 +1,43 @@
-import recommended from "./recommended";
-import strict from "./strict";
+import type { RuleConfig } from "../rules/purity";
+import type { SharedConfigurationSettings } from "../utils.pure/types";
 
 export default {
-  recommended,
-  strict,
+  recommended: {
+    rules: {
+      "functional-core/purity": [
+        // warning to allow for gradual adoption
+        "warn",
+        {
+          allowThrow: true,
+          allowIgnoreFunctionCallResult: false,
+          // can be good for performance so allowed
+          allowMutatingReduceAccumulator: true,
+          allowGlobals: {}, // dont modify default
+          allowSetters: false,
+          allowClassInstanceThisMutations: true,
+          allowFunctionWithoutReturn: true,
+        } satisfies Required<RuleConfig>,
+      ],
+    },
+  },
+
+  strict: {
+    settings: {
+      purePaths: [".*"], // all files are pure by default
+    } satisfies SharedConfigurationSettings,
+    rules: {
+      "functional-core/purity": [
+        "error",
+        {
+          allowThrow: false,
+          allowGlobals: false,
+          allowIgnoreFunctionCallResult: false,
+          allowMutatingReduceAccumulator: false,
+          allowSetters: false,
+          allowClassInstanceThisMutations: false,
+          allowFunctionWithoutReturn: false,
+        } satisfies Required<RuleConfig>,
+      ],
+    },
+  },
 };
