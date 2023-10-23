04db3d7aff06ca44fefa38dfda726ff11e3f55d8
Eli <13420359+eliasm307@users.noreply.github.com>
2023-10-23 01:03:00 +0100

    update ts ignore

--- packages/plugin/src/utils.pure/types.ts
+++ packages/plugin/src/utils.pure/types.ts
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
 import type { SourceCode } from "@typescript-eslint/utils/dist/ts-eslint";
 import type { ScopeManager } from "@typescript-eslint/scope-manager";
 
 declare module "@typescript-eslint/utils/dist/ts-eslint" {
-  // @ts-expect-error
+  // @ts-ignore [ignore name clash, we are extending the type]
   // eslint-disable-next-line
   interface SharedConfigurationSettings {
     "functional-core"?: FunctionalCoreGlobalSettings;
@@ -11,19 +12,19 @@ declare module "@typescript-eslint/utils/dist/ts-eslint" {
 
 export type { SharedConfigurationSettings } from "@typescript-eslint/utils/dist/ts-eslint";
 
-export interface FunctionalCoreGlobalSettings {
+export type FunctionalCoreGlobalSettings = {
   /**
    * A RegExp or an array of RegExp patterns that match pure file paths, where this rule will be enabled.
    * File paths including folders or files including '.pure' e.g. 'src/utils.pure/index.ts' or 'src/utils/index.pure.ts'
    * are always considered pure.
    */
   purePaths: string[] | string;
-}
+};
 
 // eslint-disable-next-line
 export type AllowGlobalsValue = boolean | { [key: string]: boolean | AllowGlobalsValue };
 
-export interface PurityRuleContext {
+export type PurityRuleContext = {
   scopeManager: ScopeManager;
   sourceCode: SourceCode;
-}
+};
